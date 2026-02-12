5eb13e15a285ef45321a888ad460192a2061c35c
tripathi0704 <shubhamtripathi0704@gmail.com>
2026-02-12 10:39:29 +0530

    Add files via upload

---
+++ FINDING EVEN ODD IN C.c
@@ -0,0 +1,21 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf(" ENTER THE  NUMBER");
+    scanf("%d",&a );
+    if (a % 2 == 0)
+    {
+        printf(" GIVEN NUMBER IS EVEN");
+
+
+    }
+    else{
+        printf(" GIVEN NUMBER IS ODD ");
+
+    }
+    return 0;
+}
