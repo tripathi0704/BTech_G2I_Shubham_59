dbc96060a8d259edd632e23389591c3f32b96acb
tripathi0704 <shubhamtripathi0704@gmail.com>
2026-02-12 10:23:05 +0530

    Add files via upload

---
+++ PROGRAM OF FINDING GREATEST OF THREE.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    printf(" ENTER THE THREE NUMBER\n");
+    scanf(" %d %d %d",&x ,&y ,&z);
+    if (a >= b && a >= c)
+    {
+        printf(" great number is a %d",a);
+    } 
+    else if (b >= c && b >= a)
+    {
+        printf(" great number is b %d", b);
+
+    }
+    else (c >= a && c >= b)
+    {
+        printf(" great number is c %d" ,c);
+    }
+}
