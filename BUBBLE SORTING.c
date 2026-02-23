64c8dae828a1c6e4c06861589e2155e901cf083d
tripathi0704 <shubhamtripathi0704@gmail.com>
2026-02-23 09:40:51 +0530

    Add files via upload

---
+++ BUBBLE SORTING.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100], n, i, j, swap; 
+
+    printf("Enter number of elements \n");
+    scanf("%d", &n);
+
+    printf("Enter %d integers \n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    // Bubble sort
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = swap;
+            }
+        }
+    }
+
+    printf("Sorted list in ascending order\n");
+    for (i = 0; i < n; i++)
+        printf("%d\n", a[i]);
+
+    return 0;
+}
